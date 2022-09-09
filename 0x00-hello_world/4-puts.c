aef93fbe8636e237ba4d64682f0b072f21c4dc34
franklo8 <franklinejeh@gmail.com>
2022-09-09 04:56:34 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+
 int main(void)
 {
+/**
+ * A program that returns a string using a standard output puts()
+ */
+
 	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
