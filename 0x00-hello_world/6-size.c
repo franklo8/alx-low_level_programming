d6304a1990f90694e0d829ca012c13bdb1364d7e
franklo8 <franklinejeh@gmail.com>
2022-09-09 06:38:51 +0000

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
-/*This code gives the size of various types of variables on your computer*/
-
+/**
+ *main -is the entry point for the program.
+ *
+ *Return:zero if no error, Non-zero value  if errors.
+ */
 int main(void)
 {
 	char c;
 	int i;
 	long int li;
-	long long int lli;
+	long long int ll;
 	float f;
-	
-	printf("The size of a char is: %lu byte(s).\n", (unsigned long)sizeof(c));
-	printf("The size of an int is: %lu byte(s).\n", (unsigned long)sizeof(i));
-	printf("The size of an long int is: %lu byte(s).\n", (unsigned long)sizeof(li));
-	printf("The size of an long long int is: %lu byte(s).\n", (unsigned long)sizeof(lli));
-	printf("The size of an float is: %lu byte(s).\n", (unsigned long)sizeof(f));
-	
+
+	printf("The size of a char is:%lu byte(s) \n", sizeof(c));
+	printf("The size of an int is:%lu byte(s) \n", sizeof(i));
+	printf("The size of an long int is:%lu byte(s) \n", sizeof(li));
+	printf("The size of an long long int is:%lu byte(s) \n", sizeof(ll));
+	printf("The size of an float is:%lu byte(s) \n", sizeof(f));
 	return (0);
 
 }
