3c5407c274950bdee114021e788a28744b190451
franklo8 <franklinejeh@gmail.com>
2022-09-12 01:42:39 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/* *
+ * main - Starting point
+ *
+ * Description: 'Prints the alphabet in lowercase'
+ *
+ * Return: lower case alphabets
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'e' && 'q')
+	{
+		puchar (alph);
+	}
+	}
+	putchar('\n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Description: 'program that prints all single digits'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
