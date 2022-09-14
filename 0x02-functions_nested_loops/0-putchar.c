a69ca587916a97af4293cae1ead9d9fc180491d2
franklo8 <franklinejeh@gmail.com>
2022-09-14 03:33:19 +0000

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - starting point 
+ *
+ * Description: prints the character _putchar
+ *
+ * return: 0
+ */
+int _putchar(char c);
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
