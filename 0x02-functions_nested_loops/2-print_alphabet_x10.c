f87e32a19b0f02c7dd9e7c65608a136c933ecabd
franklo8 <franklinejeh@gmail.com>
2022-09-15 07:43:56 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - callout function
+ *
+ * Description: prints alphabets 10 times
+ *
+ * Result: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		{
+			_putchar('\n');
+		}
+	}
+}
