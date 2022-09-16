5ed8c03ac19cae43cbc64ecd394a79a59721cb16
franklo8 <franklinejeh@gmail.com>
2022-09-16 04:42:17 -0700

    Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - tests function that prints the function give correct output
+ * Reurn: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
