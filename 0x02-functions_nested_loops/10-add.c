23babb3cf6a04669ee8165f654b0969a5e5bd54a
franklo8 <franklinejeh@gmail.com>
2022-09-17 03:54:46 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add: callout function
+ *
+ * Description: Adds two numbers
+ *
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Callout function
+ *
+ * @c: Checks for last digit
+ *
+ * Return: 0
+ */
+int print_last_digit(int c)
+{
+	return (c % 10);
+}
