dfbf37d8f191d57459fb023cd3a6d0ddc4b13efe
franklo8 <franklinejeh@gmail.com>
2022-10-11 17:41:00 +0000

    Poopy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - dog data
+ *
+ * @name: Name of the dog
+ * @age: The dog's age
+ * @owner: who is the owner
+ *
+ * Description: This is a database of dogs in my area
+ *
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+
+#endif
