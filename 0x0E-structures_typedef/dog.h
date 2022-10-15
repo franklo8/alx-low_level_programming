6975ca92803e0a4cde680ee1d3250be25c1a523d
franklo8 <franklinejeh@gmail.com>
2022-10-15 05:47:06 +0000

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,14 +11,15 @@
  * Description: This is a database of dogs in my area
  *
  */
-struct dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} my_dog;
 
-/*void init_dog(struct dog *d, char *name, float age, char *owner);*/
-/*void print_dog(struct dog *d);*/
+/*void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);*/
 
 #endif
