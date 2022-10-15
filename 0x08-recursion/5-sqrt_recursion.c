c5985b42acd96f24bf7afa2d87055c1057567a84
franklo8 <franklinejeh@gmail.com>
2022-10-15 06:15:58 +0000

    A dog will teach you unconditional love. If you can have that in your life, things would not be too bad

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/** 
+ * _sqrt_recursion - function to find square root of a number
+ *
+ * @n: number to get the square root of
+ *
+ * Return: 0
+ */
+int _sqrt_recursion(int n)
+{
+	int arr[n], i;
+	int count;
+
+	for (i = 0; i < n; i++)
+	{
+		if (arr[n] % 2 != 0);
+		return (
+	return (0);
+}
